9a305c25bf3e8f9e649a48ec06b050b47e6f3fea
Rizky Wahyu <rizkywahyufadillah3@gmail.com>
2026-04-09 14:37:13 +0700

    membuat docker file

--- docker/start.sh
+++ docker/start.sh
@@ -4,10 +4,12 @@ set -e
 chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache
 
 php artisan migrate --force
+php artisan db:seed --class=PermissionSeeder --force
+php artisan db:seed --class=PermissionGroupSeeder --force
 php artisan storage:link 2>/dev/null || true
+php artisan optimize
 
 php artisan schedule:work &
-php artisan queue:work --queue=push-multiple-notif &
 php artisan queue:work --queue=default --tries=3 --timeout=60 &
 
 exec apache2-foreground
